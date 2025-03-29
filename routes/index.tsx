de81730e6e87a9c68be201ddebef8037fd7f5763
Andrew <smol@Smol.local>
2025-03-28 19:49:50 -0500

    final name of Kerney's Diner

--- routes/index.tsx
+++ routes/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div class="px-4 py-8 mx-auto bg-[#86efac]">
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
         <h1 class="text-4xl font-bold pb-1 border-b border-gray-700">
-          Ripotle / Kerney's Diner / Taco Bell
+          Kerney's Diner
         </h1>
         <Counter count={count} />
       </div>
